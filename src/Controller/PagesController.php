9728bdaf2ac82106be0a2b0866f9caa9a7bca6ee
Vl-Wouter <woutvlae@student.arteveldehs.be>
2019-10-02 21:03:12 +0200

    update

---
+++ src/Controller/PagesController.php
@@ -0,0 +1,81 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use App\Entity\Camp;
+use DateTime;
+use Exception;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PagesController extends AbstractController
+{
+    /**
+     * @Route("/", name="index", methods={"GET"})
+     * @return Response
+     */
+    public function index() {
+        $camps = $this->getDoctrine()->getRepository(Camp::class)->findAll();
+        $featured = $this->getDoctrine()->getRepository(Camp::class)->findBy(["featured" => 1]);
+        shuffle($featured);
+
+        if(!$camps) {
+            throw $this->createNotFoundException('No camps found.');
+        }
+
+        if(!$featured) {
+            throw $this->createNotFoundException('No featured camps available');
+        }
+
+        return $this->render('pages/index.html.twig', ['camps' => $camps, 'featured' => $featured[0]]);
+    }
+
+    /**
+     * @param string $slug
+     * @Route("/camp/{slug}", name="camp", methods={"GET"})
+     * @return Response
+     */
+    public function show($slug) {
+        $camp = $this->getDoctrine()->getRepository(Camp::class)->findOneBy(["slug" => $slug]);
+
+        return $this->render('pages/camp.html.twig', ['camp' => $camp]);
+    }
+
+    /**
+     * @Route("/admin/camps/new", name="newCamp", methods={"GET", "POST"})
+     * @param Request $r
+     * @return Response
+     * @throws Exception
+     */
+    public function addCamp(Request $r) {
+        if($r->isMethod('GET')) {
+            return $this->render('cms/campForm.html.twig');
+        } else if($r->isMethod('POST')) {
+            var_dump($_POST);
+            $manager = $this->getDoctrine()->getManager();
+
+            $camp = new Camp();
+            $camp->setTitle($_POST["title"]);
+            $camp->setAuthor("TestGebruiker");
+            $camp->setQuote($_POST["quote"]);
+            $camp->setDate(new DateTime($_POST["date"]));
+            $camp->setImage($_POST["image"]);
+            $camp->setDescription($_POST["description"]);
+            $camp->setLikes(0);
+            if(array_key_exists("featured", $_POST)) {
+                $camp->setFeatured($_POST["featured"]);
+            }
+
+            $manager->persist($camp);
+
+            $manager->flush();
+
+            return $this->redirectToRoute('index', [], 301);
+
+        }
+    }
+}
---
+++ src/Entity/Comments.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CommentsRepository")
+ */
+class Comments
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $author;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $camp_id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getCampId(): ?int
+    {
+        return $this->camp_id;
+    }
+
+    public function setCampId(int $camp_id): self
+    {
+        $this->camp_id = $camp_id;
+
+        return $this;
+    }
+}
